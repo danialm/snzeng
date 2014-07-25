ed51233de22040b6e05636252d452f1d118aa7fe
danial <gdanialq@gmail.com>
2014-07-25 16:21:21 -0700

    Contact us and about us and homepage and jobs responsive fully

--- js/global.js
+++ js/global.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-    if($(window).width() < 1070){
-        $('.navigation .ham').click(function(){
-           $('.navigation .honeycomb').slideToggle(); 
-        });
-        $('.navigation .honeycomb a').click(function(){
+    $('.navigation .ham').click(function(){
+       if($(window).width() < 1070){
+            $('.navigation .honeycomb').slideToggle();
+        }
+    });
+    $('.navigation .honeycomb a').click(function(){
+        if($(window).width() < 1070){
             $('.navigation .honeycomb').slideUp(0);
-        });
-    }
+        }
+    });
 });
