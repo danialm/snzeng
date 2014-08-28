833d32a3c672a5e7cdfc14ae0871df4405e90c0b
danial <gdanialq@gmail.com>
2014-08-27 17:39:20 -0700

    working on edit project CMS

--- cms/pages/projects.php
+++ cms/pages/projects.php
@@ -6,9 +6,9 @@
 <table id="project">
     <thead>
         <tr>
-            <td class="span4">id</td>
+            <td class="span2">id</td>
             <td class="span4">name</td>
-            <td class="span4"><span title="Remove ALL" class='fa fa-minus-circle fa-2x button right error' onclick='delProject("all")'></span><span title="Add project" class='fa fa-plus-circle fa-2x right add button' onclick='showEdit("new")'></span>Thumbnail</td>
+            <td class="span6"><span title="Remove ALL" class='fa fa-minus-circle fa-2x button right error' onclick='delProject("all")'></span><span title="Add project" class='fa fa-plus-circle fa-2x right add button' onclick='showEdit("new")'></span>Thumbnail</td>
         </tr>
     </thead>
     <tbody>
@@ -24,8 +24,12 @@
             function(project){
                 if(project.length !== 0){
                     $.each(project, function(i, d){
-                        tbody.append("<tr id='"+d.id+"'><td class='span4'>"+d.id+"</td><td class='span4'>"+d.name+"</td><td class='span4'><span title='Remove' class='fa fa-minus-circle right error button' onclick='delProject("+d.id+")'></span><span title='Edit' class='fa fa-edit button right edit' onclick='showEdit("+d.id+")'></span><img src='img/projects/project"+d.id+".thumb.jpg' height='30' alt='"+d.name+" thumbnail'/></td>\n\
-                                                        <td class='span12 edit-form'><form enctype='multipart/form-data'><input type='text' name='id' value='"+d.id+"' ></input><input type='text' name='name' value='"+d.name+"' ></input><span title='Save' class='fa fa-check-circle right add button' onclick='editProject("+d.id+")'></span><input type='file' name='thumb' ></input></td></tr>");
+                        tbody.append("<tr id='"+d.id+"'><td class='span2'>"+d.id+"</td><td class='span6'>"+d.name+"</td><td class='span4'><span title='Remove' class='fa fa-minus-circle right error button' onclick='delProject("+d.id+")'></span><span title='Edit' class='fa fa-edit button right edit' onclick='showEdit("+d.id+")'></span><img src='img/projects/project"+d.id+".thumb.jpg' height='30' alt='"+d.name+" thumbnail'/></td>\n\
+                                                        <td class='edit-form'><form enctype='multipart/form-data'><span class='span2'>id:<input type='text' name='id' value='"+d.id+"' /></span><span class='span4'>name:<input type='text' name='name' value='"+d.name+"' /></span><span  class='span6'><span title='Save' class='fa fa-check-circle right add button' onclick='editProject("+d.id+")'></span>thumb:<input type='file' name='thumb' /></span>\n\
+                                                                                                                  <span class='span3'>year:<input type='text' name='year' value='"+d.year+"' /></span><span class='span3'>order:<input type='text' name='order' value='"+d.order+"' /></span><span class='span4'>address:<input type='text' name='city' value='"+d.address+"' /></span><span class='span2'><select name='type'><option value='res'>residential</option><option value='com'>comertial</option><option value='apt'>apt/condo</option><option value='fac'>facade</option><option value='res'>non-building</option></select></span>\n\
+                                                                                                                  <span class='span6'>snippet:<textarea name='snipt'>"+d.snippet+"</textarea></span><span class='span6'>description:<textarea name='description'>"+d.description+"</textarea></span>\n\
+                                                                                                                  <span class='span12'>specification:</span>    \n\
+                                                                              </form></td></tr>");
                     });
                 }else{
                     tbody.append("<tr><td class='span12'>No projects to show.</td></tr>");
